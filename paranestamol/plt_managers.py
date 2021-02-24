f1d13d2e531efc40f32b7f04037f7059a7f34f9a
Aleksandr Petrosyan <a-p-petrosyan@yandex.ru>
2021-02-24 15:52:07 +0400

    Separated the Higson logic away from the main Triangle plot logic.

--- paranestamol/plt_managers.py
+++ paranestamol/plt_managers.py
@@ -7,32 +7,47 @@ import numpy as np
 from multiprocessing import Pool
 
 
-
 class TrianglePlotter(QtCore.QObject):
     notify = QtCore.Signal(str)
     paramsChanged = QtCore.Signal()
-    reqTriangleRedraw = QtCore.Signal(object, object, object, object, object, object)
+    reqNewTriangle = QtCore.Signal(object, object, object, object, object, float)
 
     def __init__(self, parent=None):
         super().__init__(parent)
-        self.beta = 1
-        self.logL = -10000
+        self.logL = -1
         self.triCanvas = None
-        self.higCanvas = None
-        self._sample = None
         self.legends = {}
         self.samples = dict()
         self.paramsModel = None
-        self._betaCache = {}
         self._LCache = {}
-        self._thread = QtCore.QThread()
+        self._higson = HigsonPlotter()
+        self._stack = ThreadedStackBuffer()
+        self._thread = QtCore.QThread(parent=self)
         self._thread.start()
         self._worker = ThreadedPlotter()
         self._worker.moveToThread(self._thread)
-        self.reqTriangleRedraw.connect(self._worker.plot_triangle)
-        self._worker.finished.connect(self.updateTriangleFigure)
-        
+        self.reqNewTriangle.connect(self._stack.push)
+        self._stack.popped.connect(self._worker.plot_triangle)
+        self._worker.finished.connect(self.cacheTriangleFigure)
 
+
+    @property
+    def beta(self):
+        return self._higson.beta
+
+    @beta.setter
+    def beta(self, other):
+        self._higson.beta = other
+
+
+    @property
+    def higCanvas(self):
+        return self._higson.higCanvas()
+
+    @higCanvas.setter
+    def higCanvas(self, other):
+        self._higson.higCanvas = other
+    
     @property
     def params(self):
         return list(self.paramsModel.displayNames)
@@ -41,127 +56,169 @@ class TrianglePlotter(QtCore.QObject):
     def tex(self):
         return self.paramsModel.tex
 
+    @QtCore.Slot(float, object)
+    def cacheTriangleFigure(self, logL, fig):
+        self._stack.pop()
+        self._LCache[logL] = fig
+        if self.logL == logL:
+            self._updateTriangleFigure(self._LCache[logL])
+            print(f'displaying {logL}')
+
+        
     @QtCore.Slot(object)
-    def updateTriangleFigure(self, fig):
+    def _updateTriangleFigure(self, fig):
         self.triCanvas.figure = fig
         fig.set_canvas(self.triCanvas)
         self.triCanvas.draw_idle()
 
-    def _update_triangle(self, post=None):
-        if post is None:
-            post = lambda x: x
+    def request_update_triangle(self, post=None, logL=-1):
         fig = self.triCanvas.figure
         figsize = fig.get_figwidth(), fig.get_figheight()
-        self.reqTriangleRedraw.emit(Figure(figsize=figsize), self.params, self.tex, self.samples, self.legends, post)
-        # fig = updateTrianglePlot(Figure(figsize=figsize), self.params, self.tex,
-        #                          self.samples, self.legends, post)
-        # self.updateTriangleFigure(fig)
-        # self._update_higson()
+        self.reqNewTriangle.emit(Figure(figsize=figsize), self.params, self.tex, self.samples, self.legends, logL)
 
-    def _update_higson(self):
-        fig = self.higCanvas.figure
-        fig.clear()
-        ax = fig.gca()
-        ax.set_xlabel(r'$\log X$')
-        ax.set_ylabel(r'$LX$')
-        for x in self.samples:
-            with np.errstate(divide='ignore'):
-                logX = np.log(self.samples[x].nlive / (self.samples[x].nlive+1)).cumsum()
-            LX = self.samples[x].logL/self.beta + logX
-            LX = np.exp(LX-LX.max())
-            ax.plot(logX, LX, color=self.legends[x].color)
-        self.higCanvas.draw_idle()
+
+    def _update_higson(self, *args):
+        self._higson._update_higson(*args)
 
     @QtCore.Slot()
     @QtCore.Slot(object, object)
     def invalidateCache(self, *args):
-        self.notify.emit('Invalidating cache')
-        figsize = self.triCanvas.figure.get_figwidth(), self.triCanvas.figure.get_figheight()
+        self._stack.clear_buffer()
+        old_cache = self._LCache
         self._LCache = {}
-
+        for x in old_cache:
+            self.request_update_triangle(logL=x)
+        self._stack.pop()
         
-
-    def _cache_figure(self, logL, *args):
-        self._LCache[self.logL] = self.triCanvas.figure
+    @QtCore.Slot(float)
+    def changeLogL(self, logL, *args):
         self.logL = logL
         if self.logL in self._LCache:
             self.triCanvas.figure = self._LCache[self.logL]
             self._LCache[self.logL].set_canvas(self.triCanvas)
             self.triCanvas.draw_idle()
         else:
-            self._update_triangle(lambda x: x.live_points(self.logL))
-            self._LCache[self.logL] = self.triCanvas.figure
-
-    def _cache_rgb(self, logL, *args):
-        # print('caching what we have')
-        rgb = self.triCanvas.tostring_rgb()
-        ncols, nrows = self.triCanvas.get_width_height()
-        self._LCache[self.logL] = np.fromstring(rgb, dtype=np.uint8).reshape(nrows, ncols, 3)
-        self.logL = logL
-        if self.logL in self._LCache:
-            # print(f're-using for {self.logL}')
-            ax = self.triCanvas.figure.gca()
-            ax.imshow(self._LCache[self.logL])
-            self.triCanvas.draw_idle()
-        else:
-            # print(f'drawing {self.logL}')
-            self._update_triangle(lambda x: x.live_points(self.logL))
-        
-    @QtCore.Slot(float)
-    def changeLogL(self, logL, *args):
-        self._cache_figure(logL, *args)
-        # self._cache_rgb(logL, *args)
+            self.request_update_triangle(logL= self.logL)
 
     @QtCore.Slot(float)
     def changeTemperature(self, beta, *args):
         self.beta = beta
-        self._update_triangle()
-        self._update_higson()
+        self._update_higson(self.samples, self.legends)
 
     @QtCore.Slot()
     @QtCore.Slot(object)
     @QtCore.Slot(object, object)
     def reDraw(self, samples=None, legends=None, *args):
-        # print(samples, legends)
         if samples is not None and not isinstance(samples, QtCore.QModelIndex):
             self.samples = samples
-        if legends is None:
-            if self.legends is None:
+        if legends is None and self.legends is None:
                 for k in self.samples:
                     self.legends[k] = Legend(title=k)
         else:
             # Fuck the Duck typing. 
             if not isinstance(legends, QtCore.QModelIndex):
                 self.legends = legends
-        self.notify.emit('Full repaint...')
-        self._update_triangle()
-        self._update_higson()
-        self.notify.emit('Fully repainted.')
+        self._stack.pop()
+        self._update_higson(self.samples, self.legends)
 
+
+class HigsonPlotter(QtCore.QObject):
+    def __init__(self,  parent=None):
+        super().__init__(parent)
+        self._beta = 1
+
+    @property
+    def beta(self):
+        return self._beta
+
+    @beta.setter
+    def beta(self, other):
+        self._beta = other
+
+    @property
+    def higCanvas(self):
+        return self._higCanvas
+
+    @higCanvas.setter
+    def higCanvas(self, other):
+        self._higCanvas = other
+        fig = self.higCanvas.figure
+        ax = fig.gca()
+        ax.cla()
+        ax.xaxis.set_tick_params(labeltop='on')
+        ax.xaxis.set_tick_params(labelbottom=False)
+        ax.set_xlabel(r'$\log X$')
+        ax.set_ylabel(r'$LX$', labelpad=-30)
+        fig.set_tight_layout({'pad': 0})
+
+    def _update_higson(self, samples, legends):
+        ax = self.higCanvas.figure.gca()
+        try:
+            ax.lines[0].remove()
+        except:
+            pass
+        for x in samples:
+            with np.errstate(divide='ignore'):
+                logX = np.log(samples[x].nlive / (samples[x].nlive+1)).cumsum()
+            LXi = samples[x].logL/self.beta + logX
+            LX = np.exp(LXi-LXi.max())
+            ax.plot(logX[::-1], LX, color=legends[x].color)
+        self.higCanvas.draw_idle()
 
 class ThreadedPlotter(QtCore.QObject):
-    finished = QtCore.Signal(object)
+    finished = QtCore.Signal(float, object)
 
-    @QtCore.Slot(object, object, object, object, object, object)
+    @QtCore.Slot(object, object, object, object, object, float)
     def plot_triangle(self, *args):
+        self.busy=True
         fig = updateTrianglePlot(*args)
-        self.finished.emit(fig)
+        self.finished.emit(args[-1], fig)
+        self.busy=False
+
+
+class ThreadedStackBuffer(QtCore.QObject):
+    popped = QtCore.Signal(object, object, object, object, object, float)
+
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self._buffer = []
+        self.autopop = True
+
+    def clear_buffer(self):
+        self._buffer = []
+
+    def pop(self):
+        try:
+            print(f'pop {self._buffer[-1][-1]}')
+            self.autopop=False
+            self.popped.emit(*self._buffer.pop())
+        except:
+            self.autopop=True
+
+        
+    @QtCore.Slot(object, object, object, object, object, float)
+    def push(self, *args):
+        print(f'push {args[-1]}')
+        self._buffer.append(args)
+        if self.autopop:
+            self.pop()
         
 
 
-def updateTrianglePlot(figure, params, tex, samples, legends, postprocess):
+def updateTrianglePlot(figure, params, tex, samples, legends, logL):
     figure.clear()
     figure, axes = make_2d_axes(params, tex=tex, fig=figure, upper=False)
     for x in samples:
-        postprocess(samples[x])\
-                  .plot_2d(axes,
-                           alpha=legends[x].alpha,
-                           color=legends[x].color,
-                           label=legends[x].title,
-                           types={
-                               'lower': 'scatter',
-                               'diagonal': 'hist',
-                           })
+        samples[x]\
+            .live_points(logL)\
+            .plot_2d(axes,
+                     alpha=legends[x].alpha,
+                     color=legends[x].color,
+                     label=legends[x].title,
+                     types={
+                         'lower': 'scatter',
+                         'diagonal': 'hist',
+                     })
     handles, labels = axes[params[0]][params[1]]\
         .get_legend_handles_labels()
     figure.legend(handles, labels)
