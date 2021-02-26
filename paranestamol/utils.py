1249655f7d53ee332b9f6c88c76c70a2b75adc20
Aleksandr Petrosyan <a-p-petrosyan@yandex.ru>
2021-02-26 12:08:48 +0400

    Bugfix.

--- paranestamol/utils.py
+++ paranestamol/utils.py
@@ -1,11 +1,9 @@
 from os.path import splitext
 from matplotlib.pyplot import rcParams
 
-
 def cleanupFileRoot(file_root):
     ret = file_root.replace('file://', '', 1)
     exts = ['.stats', '.resume', '.paramnames', '.inputparams', '.ranges']
-    # Make sure to put the substrings later, so that the longer part can be picked up.
     ends = ['_equal_weights', '_dead-birth', '_dead','_phys_live-birth',  '_phys_live']
     root, ext = splitext(ret)
     if ext in exts:
@@ -17,8 +15,8 @@ def cleanupFileRoot(file_root):
                 return rt, end+ext
             except ValueError:
                 pass
-
-
+    else:
+       return file_root, ''         
 
 
 class Legend:
